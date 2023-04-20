e693179b5816005efea8f40461ad17bb68a35137
untari <sastraembun0306@gmail.com>
2023-04-20 12:41:38 +0800

    add some logic in user option

--- main.cpp
+++ main.cpp
@@ -22,5 +22,33 @@ int main(){
     std::cin >> userOption;
     std::cout << "you choose: " << userOption << std::endl;
 
+    if (userOption == 0) //bad input
+    {
+        std::cout << "invalide choice. choose 1-6" << std::endl;
+    }
+    if (userOption == 1) //bad input
+    {
+        std::cout << "help - your aim is to make money. analyse the market and bids the market" << std::endl;
+    }
+    if (userOption == 2) //bad input
+    {
+        std::cout << "market looks good" << std::endl;
+    }
+    if (userOption == 3) //bad input
+    {
+        std::cout << "mark and offer - enter the amount " << std::endl;
+    }
+    if (userOption == 4) //bad input
+    {
+        std::cout << "make a bid - enter the amount" << std::endl;
+    }
+    if (userOption == 5) //bad input
+    {
+        std::cout << "your wallet is empty" << std::endl;
+    }
+    if (userOption == 6) //bad input
+    {
+        std::cout << "going to next time frame" << std::endl;
+    }
     return 0;
 }
