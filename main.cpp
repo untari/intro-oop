a329626f5ba215063559a32d028a632bff48b21d
untari <sastraembun0306@gmail.com>
2023-05-13 15:08:08 +0700

    delete merklerex file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 
 // main function
 int main(){
+    while(true){
+
+        printMenu();
+        int userOption = getUserOption();
+        processUserOption(userOption);
+    }
+    return 0;
+}
+
+int getUserOption(){
+
+    int userOption(); 
+
+    std::cout << "Type in 1-6" << std::endl;
+    std::cin >> userOption;
+    std::cout << "you choose: " << userOption << std::endl;
+    return userOption;
+}
+
+void printMenu(){
     // 1. print help
     std::cout << "1: print help" << std::endl;
     // 2. print exchange status
@@ -16,12 +36,10 @@ int main(){
     std::cout << "6: continue" << std::endl;
 
     std::cout << "===========" << std::endl;
-    std::cout << "Type in 1-6" << std::endl;
+}
 
-    int userOption;
-    std::cin >> userOption;
-    std::cout << "you choose: " << userOption << std::endl;
-
+void processUserOption(int userOption)
+{
     if (userOption == 0) //bad input
     {
         std::cout << "invalide choice. choose 1-6" << std::endl;
@@ -50,5 +68,4 @@ int main(){
     {
         std::cout << "going to next time frame" << std::endl;
     }
-    return 0;
 }
