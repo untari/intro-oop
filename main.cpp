63245a4400a6785e3e2d6707927291e8f90e9be9
untari <sastraembun0306@gmail.com>
2023-05-15 19:26:47 +0700

    three ways to iterate over a vector

--- main.cpp
+++ main.cpp
@@ -166,14 +166,23 @@ int main(){
     // order1.product = "BTC/USDT";
     // order1.orderType = OrderBookType::bid;
 
-    // print it out
+    // print it out 1 value
     // std::cout << "the price is " << orders[1].price << std::endl;
 
     // to print out everything using iteration
-    for (OrderBookEntry order : orders){
+    for (OrderBookEntry& order : orders){
         std::cout << "the price is " << order.price << std::endl;
     }
 
+    // using arrays syntax with vectors
+    for(unsigned i = 0; i < orders.size(); ++i){
+        std::cout << "the price is " << orders[i].price << std::endl;
+    }
+
+    // or using object style
+    for(unsigned i = 0; i < orders.size(); ++i){
+        std::cout << "the price is " << orders.at(i).price << std::endl;
+    }
 }
 
 
