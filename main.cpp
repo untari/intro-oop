6d5d4306d1a79b6566404311e94fac2833a465f2
untari <sastraembun0306@gmail.com>
2023-05-15 18:58:17 +0700

    initialisation list in constructor

--- main.cpp
+++ main.cpp
@@ -11,13 +11,11 @@ class OrderBookEntry{
 
         // define constructors
         OrderBookEntry(double _price, double _amount, std::string _timestamp, 
-        std::string _product, OrderBookType _orderType){
-           
-           price = _price;
-           amount = _amount;
-           timestamp = _timestamp;
-           product = _product;
-           orderType = _orderType;
+        std::string _product, OrderBookType _orderType)
+        : price(_price), amount(_amount), timestamp(_timestamp), 
+        product(_product), orderType(_orderType)
+        {
+
         }
 
         // declare the variables
