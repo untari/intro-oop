1bdc0ccdb09d5f988e917ebdfee0ae57fee92d84
untari <sastraembun0306@gmail.com>
2023-05-15 17:38:16 +0700

    store longer number

--- test.cpp
+++ test.cpp
@@ -3,9 +3,10 @@
 
 int main()
 {
-    unsigned short i = 6444; 
+    signed long i = 644444; 
 
     std::cout << "sizeof unsigned short " << sizeof(unsigned short) << std::endl;
+    std::cout << "sizeof unsigned long " << sizeof(unsigned long) << std::endl;
     std::cout << "i contains " << i << std::endl;
 
     return 0;
