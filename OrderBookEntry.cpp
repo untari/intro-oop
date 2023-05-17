9cfc0c622243e8e3c79e648a15d2dcc814da5ec5
untari <sastraembun0306@gmail.com>
2023-05-17 15:42:11 +0700

    used orderbookentry header in the main.cpp

---
+++ OrderBookEntry.cpp
@@ -0,0 +1,11 @@
+#include "OrderBookEntry.h"
+
+// define constructors
+OrderBookEntry::OrderBookEntry(double _price, double _amount, std::string _timestamp, 
+std::string _product, OrderBookType _orderType)
+: price(_price), amount(_amount), timestamp(_timestamp), 
+product(_product), orderType(_orderType)
+{
+
+}
+
