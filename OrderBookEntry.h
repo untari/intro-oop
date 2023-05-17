7de6b0e28a4e62bb463e85f2c0673f16da8a66c6
untari <sastraembun0306@gmail.com>
2023-05-17 15:28:26 +0700

    moved OrderBookEntry class to header file

---
+++ OrderBookEntry.h
@@ -0,0 +1,25 @@
+#include  <string>
+
+// define enum class(it's a way of specifying a new type with 2 values)
+enum class OrderBookType{bid, ask};
+
+// define class
+class OrderBookEntry{
+    public:
+
+        // define constructors
+        OrderBookEntry(double _price, double _amount, std::string _timestamp, 
+        std::string _product, OrderBookType _orderType)
+        : price(_price), amount(_amount), timestamp(_timestamp), 
+        product(_product), orderType(_orderType)
+        {
+
+        }
+
+        // declare the variables
+        double price;
+        double amount;
+        std::string timestamp;
+        std::string product;
+        OrderBookType orderType;
+};
--- main.cpp
+++ main.cpp
@@ -1,30 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-// define enum class(it's a way of specifying a new type with 2 values)
-enum class OrderBookType{bid, ask};
-
-// define class
-class OrderBookEntry{
-    public:
-
-        // define constructors
-        OrderBookEntry(double _price, double _amount, std::string _timestamp, 
-        std::string _product, OrderBookType _orderType)
-        : price(_price), amount(_amount), timestamp(_timestamp), 
-        product(_product), orderType(_orderType)
-        {
-
-        }
-
-        // declare the variables
-        double price;
-        double amount;
-        std::string timestamp;
-        std::string product;
-        OrderBookType orderType;
-};
+#include "OrderBookEntry.h"
 
 void printMenu(){
     // 1. print help
